f30fee79fb0095756be6db1e50f7da8c9903dfdc
Ujjwal_gupta_ <120356026+Ujjwalgupta12@users.noreply.github.com>
2025-06-25 08:46:43 +0530

    Update index.js

--- React/Task-Manager/server/database/index.js
+++ React/Task-Manager/server/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://sangammukherjee2022:sangammukherjee2023@cluster0.fiyruuq.mongodb.net/"
+    "mongodb+srv://hellogupta2003:MongoDb%402003@cluster0.fiyruuq.mongodb.net/"
   )
   .then(() => console.log("MongoDB Connection successfull"))
   .catch((error) => console.log(`Error occured: ${error}`));
